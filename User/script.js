9c65e481842af7888452ef1520ec90a613949f5a
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2025-09-10 10:19:50 +0530

    done

--- User/script.js
+++ User/script.js
@@ -1,14 +1,14 @@
 var swiper = new Swiper(".home", {
     spaceBetween: 30,
     centeredSlides: true,
-    // autoplay: {
-    //   delay: 2500,
-    //   disableOnInteraction: false,
-    // },
-    // pagination: {
-    //   el: ".swiper-pagination", 
-    //   clickable: true,
-    // },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination", 
+      clickable: true,
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
